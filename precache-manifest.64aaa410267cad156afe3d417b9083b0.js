0f50e455b45cb6c3cc79c5597bfd3fa36ff1cead
Estelle Choi <estele.choi@gmail.com>
2020-10-10 17:14:36 +0900

    Updates

---
+++ precache-manifest.64aaa410267cad156afe3d417b9083b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aa51bf4ea96d18d8061c3e1cc2520cd",
+    "url": "/ruby-potato/index.html"
+  },
+  {
+    "revision": "50981682c1155240c53c",
+    "url": "/ruby-potato/static/css/main.c470a59e.chunk.css"
+  },
+  {
+    "revision": "e323d5838d8fbe5a03ce",
+    "url": "/ruby-potato/static/js/2.1a7595c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ruby-potato/static/js/2.1a7595c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50981682c1155240c53c",
+    "url": "/ruby-potato/static/js/main.8c1f3e44.chunk.js"
+  },
+  {
+    "revision": "aef17f476fe1cf53148b",
+    "url": "/ruby-potato/static/js/runtime-main.3cff5e1e.js"
+  }
+]);

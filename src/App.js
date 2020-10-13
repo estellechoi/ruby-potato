0d0360be59d9a64bc686725d54e5fa0602a64cb5
Estelle Choi <estele.choi@gmail.com>
2020-10-13 18:35:45 +0900

    temp code added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,22 @@ import Page from "./router/Page";
 import Navigation from "./components/Navigation";
 
 function App() {
+	// const pages = [
+	// 	{
+	// 		id: 1,
+	// 		name: "Github",
+	// 		url: "https://github.com/estellechoi",
+	// 	},
+	// ];
+
+	// // When manipulating some variables, without re-rendering, just use useRef instead of useState.
+	// const nextId = useRef(4);
+
+	// const onCreate = () => {
+	// 	console.log(nextId.current); // 4
+	// 	nextId.current += 1;
+	// };
+
 	return (
 		<>
 			<HashRouter>
